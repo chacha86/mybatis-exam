afd9c273dd092bbca066139cab8ce74491018fb0
woooyoung <axdsw121@gmail.com>
2024-10-01 20:18:08 +0900

    thymleaf paramter additional

--- src/main/java/com/example/basic/TestController.java
+++ src/main/java/com/example/basic/TestController.java
@@ -1,12 +1,19 @@
 package com.example.basic;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class TestController {
+
+    private final ArticleDao articleDao;
 
     @GetMapping("/test/var")
     public String var() {
@@ -27,8 +34,21 @@ public class TestController {
     public String param(Model model) {
 
         int myNumber = 10;
+        String myString = "hello";
+
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("banana");
+        fruits.add("orange");
+        fruits.add("apple");
+
+
+        Article article = articleDao.findById(1L);
 
         model.addAttribute("myNumber", myNumber);
+        model.addAttribute("myString", myString);
+        model.addAttribute("fruits", fruits);
+        model.addAttribute("article", article);
 
         return "test/param";
     }
