98d139d5d5fb5a6ddfee0af3b306104302e6fa5a
chacha86 <taejincha0619@gmail.com>
2024-10-14 20:47:50 +0900

    admin example 3,4 apply

--- src/main/java/com/example/basic/domain/admin/AdminController.java
+++ src/main/java/com/example/basic/domain/admin/AdminController.java
@@ -1,15 +1,28 @@
 package com.example.basic.domain.admin;
 
+import com.example.basic.global.ReqResHandler;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
 
+    private final ReqResHandler reqResHandler;
+
     @GetMapping("/admin/main")
-    public String main() {
+    public String main(HttpServletRequest request, Model model) {
+        Cookie targetCookie = reqResHandler.getLoginCookie(request);
+
+        if(targetCookie == null) {
+            throw new RuntimeException("관리자 계정으로 로그인 해야만 사용 가능합니다.");
+        }
+
+        model.addAttribute("loginedUser", targetCookie.getValue());
         return "admin/main";
     }
 
