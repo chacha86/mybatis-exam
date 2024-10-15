19dc9eb0a729c66de1c205ecbd965d3b1409c6d8
chacha86 <taejincha0619@gmail.com>
2024-10-15 19:30:52 +0900

    member account add for test

--- src/main/java/com/example/basic/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/basic/domain/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.basic.domain.auth.controller;
 
+import com.example.basic.domain.auth.entity.Member;
 import com.example.basic.global.ReqResHandler;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,11 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -49,19 +54,41 @@ public class AuthController {
 
     @PostMapping("/login")
     public String login(@Valid LoginForm loginForm, HttpServletResponse response) {
-        String dbUser = "hong";
-        String dbPass = "1234";
-        String dbRole = "admin"; // normal, admin
 
-        // 로그인 실패 -> 아이디가 잘못됐거나, 비밀번호 틀렸을 경우
-        if(!dbUser.equals(loginForm.username) || !dbPass.equals(loginForm.password)) {
+        List<Member> memberList = new ArrayList<>();
+
+        Member member1 = Member.builder()
+                .username("hong")
+                .password("1234")
+                .role("admin")
+                .build();
+
+        Member member2 = Member.builder()
+                .username("kim")
+                .password("qwer")
+                .role("normal")
+                .build();
+
+        memberList.add(member1);
+        memberList.add(member2);
+
+        Member targetMember = null;
+
+        for(Member member : memberList) {
+            if(member.getUsername().equals(loginForm.username) && member.getPassword().equals(loginForm.password)) {
+                targetMember = member;
+                break;
+            }
+        }
+
+        if(targetMember == null) {
             return "login-fail";
         }
 
         // loginUser 쿠폰을 발행. 쿠폰 값은 username으로 해주세요.
         // 로그인 성공
         Cookie cookie = new Cookie("loginUser", loginForm.username);
-        Cookie role = new Cookie("role", dbRole);
+        Cookie role = new Cookie("role", targetMember.getRole());
 
         // 쿠키의 유효 시간 설정 (초 단위, 1시간 유효) => 로그인 만료 시간
         cookie.setMaxAge(60 * 60);
