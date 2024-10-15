c3cd78e6e526d78399e43366bb40e1913fb63259
chacha86 <taejincha0619@gmail.com>
2024-10-15 21:36:16 +0900

    admin session apply

--- src/main/java/com/example/basic/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/basic/domain/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.basic.domain.admin.controller;
 import com.example.basic.global.ReqResHandler;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,22 +16,21 @@ public class AdminController {
     private final ReqResHandler reqResHandler;
 
     @GetMapping("/admin/main")
-    public String main(HttpServletRequest request, Model model) {
-        Cookie targetCookie = reqResHandler.getCookieByName(request, "loginUser");
+    public String main(HttpServletRequest request, Model model, HttpSession session) {
 
-        if(targetCookie == null) {
+        String username = (String)session.getAttribute("loginUser");
+
+        if(username == null) {
             throw new RuntimeException("관리자 계정으로 로그인 해야만 사용 가능합니다.");
         }
 
         // normal, admin
-        Cookie role = reqResHandler.getCookieByName(request, "role");
+        String role = (String)session.getAttribute("role");
         
-        if(!role.getValue().equals("admin")) {
+        if(!role.equals("admin")) {
             throw new RuntimeException("관리자 권한만 접근 가능합니다.");
         }
 
-        model.addAttribute("role", role.getValue()); // 웹 관련 처리
-        model.addAttribute("loginedUser", targetCookie.getValue());
         return "admin/main";
     }
 
