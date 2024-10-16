f6434cb0a0ec2cb73cf36cc52a2c5cfdb4284e30
chacha86 <taejincha0619@gmail.com>
2024-10-16 19:26:09 +0900

    admin menu stat, user add

--- src/main/java/com/example/basic/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/basic/domain/admin/controller/AdminController.java
@@ -8,14 +8,54 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
 
     private final ReqResHandler reqResHandler;
 
-    @GetMapping("/admin/main")
+    @GetMapping("/stat")
+    public String stat(HttpSession session) {
+
+        String username = (String)session.getAttribute("loginUser");
+
+        if(username == null) {
+            throw new RuntimeException("관리자 계정으로 로그인 해야만 사용 가능합니다.");
+        }
+
+        // normal, admin
+        String role = (String)session.getAttribute("role");
+
+        if(!role.equals("admin")) {
+            throw new RuntimeException("관리자 권한만 접근 가능합니다.");
+        }
+
+        return "admin/stat";
+    }
+
+    @GetMapping("/user")
+    public String user(HttpSession session) {
+
+        String username = (String)session.getAttribute("loginUser");
+
+        if(username == null) {
+            throw new RuntimeException("관리자 계정으로 로그인 해야만 사용 가능합니다.");
+        }
+
+        // normal, admin
+        String role = (String)session.getAttribute("role");
+
+        if(!role.equals("admin")) {
+            throw new RuntimeException("관리자 권한만 접근 가능합니다.");
+        }
+        return "admin/user";
+    }
+
+
+    @GetMapping("/main")
     public String main(HttpServletRequest request, Model model, HttpSession session) {
 
         String username = (String)session.getAttribute("loginUser");
