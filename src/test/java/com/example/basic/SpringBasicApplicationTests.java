a27e100549e853cf918dbf266ab9ead7f4af3304
chacha86 <taejincha0619@gmail.com>
2024-10-17 20:26:58 +0900

    jpa findById, delete, update test

--- src/test/java/com/example/basic/SpringBasicApplicationTests.java
+++ src/test/java/com/example/basic/SpringBasicApplicationTests.java
@@ -3,12 +3,14 @@ package com.example.basic;
 import com.example.basic.domain.auth.dao.MemberDao;
 import com.example.basic.domain.auth.entity.Member;
 import com.example.basic.domain.auth.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringBasicApplicationTests {
@@ -20,6 +22,56 @@ class SpringBasicApplicationTests {
 	private MemberRepository memberRepository;
 
 	@Test
+	@DisplayName("회원 수정 - save")
+	void t7() {
+
+		// 수정할 대상을 먼저 찾아오기
+
+		Optional<Member> memberOpt = memberRepository.findById("kim");
+		Member member = memberOpt.get();
+
+		System.out.println(member.getUsername() + " : " + member.getRole());
+
+		member.setRole("admin"); // 엔터티의 값을 바꾸고
+		memberRepository.save(member); // 엔터티 다시 저장
+
+	}
+
+
+	@Test
+	@DisplayName("회원 삭제 - delete, deleteById")
+	void t6() {
+
+		// id로 삭제
+//		memberRepository.deleteById("kim");
+
+		Optional<Member> memberOpt = memberRepository.findById("kim");
+		Member member = memberOpt.get();
+
+		// entity로 삭제
+		memberRepository.delete(member);
+	}
+
+
+
+	@Test
+	@DisplayName("회원 단건 조회 - findById")
+	void t5() {
+		Optional<Member> memberOpt = memberRepository.findById("kim");
+
+		if(memberOpt.isPresent()) {
+			Member member = memberOpt.get();
+
+			System.out.println(member.getUsername());
+			System.out.println(member.getPassword());
+			System.out.println(member.getRole());
+		}
+
+
+	}
+
+	@Test
+	@DisplayName("회원 전체 조회 - findAll")
 	void t4() {
 		List<Member> memberList = memberRepository.findAll();
 
@@ -29,8 +81,8 @@ class SpringBasicApplicationTests {
 	}
 
 	@Test
+	@DisplayName("회원 저장 - save")
 	void t3() {
-
 		Member member2 = Member.builder()
 				.username("kim")
 				.password("qwer")
